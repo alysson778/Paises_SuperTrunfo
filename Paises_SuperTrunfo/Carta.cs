afd04b0d0629879f5bb8d4103b89d6f589a3533a
alysson778 <alyssonmizareis@gmail.com>
2026-09-14 21:18:03 -0300

    Lógica do jogo add

--- Paises_SuperTrunfo/Carta.cs
+++ Paises_SuperTrunfo/Carta.cs
@@ -7,12 +7,24 @@ namespace Paises_SuperTrunfo
     public class Carta
     {
         public string Nacao { get; set; } = string.Empty;
-        public decimal Pib {  get; set; }
-        public int Tamanho { get; set;}
+        public decimal Pib { get; set; }
+        public int Tamanho { get; set; }
         public long Populacao { get; set; }
         public float Idh { get; set; }
         public bool IsSuperTrunfo { get; set; }
         public string TipoCarta { get; set; } = string.Empty;
 
+        public decimal ObterValorAtributo(int opcao)
+        {
+            return opcao switch
+            {
+                1 => Pib,
+                2 => (decimal)Tamanho,
+                3 => (decimal)Populacao,
+                4 => (decimal)Idh,
+                _ => 0m
+            };
+
+        }
     }
 }
--- Paises_SuperTrunfo/Program.cs
+++ Paises_SuperTrunfo/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 
 using Paises_SuperTrunfo;
-    
+            
             // 1. Instancia o baralho
             Baralho baralho = new Baralho();
 
@@ -11,6 +11,8 @@ using Paises_SuperTrunfo;
             baralho.AdicionarCarta(new Carta { Nacao = "Alemanha", Pib = 4000000000000m, Tamanho = 357022, Populacao = 83000000, Idh = 0.942f, IsSuperTrunfo = false, TipoCarta = "2A" });
             baralho.AdicionarCarta(new Carta { Nacao = "Japão", Pib = 4900000000000m, Tamanho = 377975, Populacao = 125000000, Idh = 0.925f, IsSuperTrunfo = false, TipoCarta = "1B" });
             baralho.AdicionarCarta(new Carta { Nacao = "Estados Unidos", Pib = 23000000000000m, Tamanho = 9833517, Populacao = 331000000, Idh = 0.921f, IsSuperTrunfo = true, TipoCarta = "ST" });
+                        
+
 
             Console.WriteLine("--- Cartas antes de embaralhar ---");
             foreach (var carta in baralho.Cartas)
@@ -39,7 +41,61 @@ using Paises_SuperTrunfo;
             Console.WriteLine("\n--- Cartas do Jogador 1 ---");
             foreach (var carta in jogadores[0].Cartas)
             {
-                Console.WriteLine($"{carta.TipoCarta} - {carta.Nacao}");
+                Console.WriteLine($"{carta.TipoCarta} - {carta.Nacao} - U$ {carta.Pib} - {carta.Tamanho} - {carta.Populacao} - {carta.Idh}");
             }
-        
-   
+
+            
+            Console.WriteLine("----Escollha um dos atributos----");
+            Console.WriteLine("""
+                1 - PIB 
+                2 - Tamanho do território
+                3 - População
+                4 - IDH
+                """);
+            int opcao = int.Parse(Console.ReadLine());
+            if (jogadores[0].Cartas[0].IsSuperTrunfo==true || jogadores[1].Cartas[0].IsSuperTrunfo==true )
+            {
+                char TipoCartaJogador1 = jogadores[0].Cartas[0].TipoCarta[^1];
+                char TipoCartaJogador2 = jogadores[1].Cartas[0].TipoCarta[^1];
+                if (TipoCartaJogador1 == 'T')
+                    {
+                    if (TipoCartaJogador2 == 'A')
+                        {
+                        Console.WriteLine("O Jogador 2 venceu!");
+                        }
+                    else 
+                        {
+                         Console.WriteLine("O Jogador 1 venceu1");
+                         }
+                    }
+                else if (TipoCartaJogador2 == 'T')
+                    { 
+                    if (TipoCartaJogador1 == 'A')
+                {
+                    Console.WriteLine("O Jogador 1 venceu!");
+                }
+                    else
+                {
+                    Console.WriteLine("O Jogador 2 venceu1");
+                }
+                    }
+                }
+
+            else { 
+                Decimal AtributoJogador1;
+                Decimal AtributoJogador2;
+                AtributoJogador1 = jogadores[0].Cartas[0].ObterValorAtributo(opcao);
+                AtributoJogador2 = jogadores[1].Cartas[0].ObterValorAtributo(opcao);
+
+                if (AtributoJogador1 > AtributoJogador2)
+                {
+                    Console.WriteLine("O Jogador 1 venceu!");
+                }
+                else
+                {
+                    Console.WriteLine("O Jogador 2 venceu!");
+                }
+      }
+
+
+            
