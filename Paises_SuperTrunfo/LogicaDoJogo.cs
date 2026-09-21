baf2a19168d9a50be8f9a96409bb7fe29a9a07d6
alysson778 <alyssonmizareis@gmail.com>
2026-09-20 21:16:56 -0300

    Nova classe com a lógica do jogo add

---
+++ Paises_SuperTrunfo/LogicaDoJogo.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paises_SuperTrunfo
+{
+    public class LogicaDoJogo
+    {
+        List<Jogador> jogadores = new List<Jogador>
+            {
+                new Jogador { Nome = "Jogador 1" },
+                new Jogador { Nome = "Jogador 2" }
+            };
+
+        int opcao;
+
+
+        
+     public void MostrarBaralho(Baralho baralho) 
+     {
+            Console.WriteLine("--- Cartas antes de embaralhar ---");
+            foreach (var carta in baralho.Cartas)
+            {
+                Console.WriteLine($"{carta.TipoCarta} - {carta.Nacao}");
+            }
+
+            // 3. Embaralha as cartas
+            baralho.Embaralhar();
+
+            Console.WriteLine("\n--- Cartas depois de embaralhar ---");
+            foreach (var carta in baralho.Cartas)
+            {
+                Console.WriteLine($"{carta.TipoCarta} - {carta.Nacao}");
+            }
+
+     }
+
+        public void VerificarCartas(Carta cartaJogador1, Carta cartaJogador2, int opcao)
+        {
+            if (cartaJogador1.IsSuperTrunfo || cartaJogador2.IsSuperTrunfo)
+            {
+
+                char TipoCartaJogador1 = cartaJogador1.TipoCarta[^1];
+                char TipoCartaJogador2 = cartaJogador2.TipoCarta[^1];
+                if (TipoCartaJogador1 == 'T')
+                {
+                    if (TipoCartaJogador2 == 'A')
+                    {
+                        Console.WriteLine($"\nO Jogador 2 venceu! (Super Trunfo perde para carta A - {cartaJogador2.TipoCarta})");
+                
+
+                        // Jogador 2 Recebe as cartas
+                        jogadores[1].Cartas.Add(cartaJogador1);
+                        jogadores[1].Cartas.Add(cartaJogador2);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nO Jogador 1 venceu com o Super Trunfo!");
+                        // Jogador 1 Recebe as cartas
+                        jogadores[0].Cartas.Add(cartaJogador2);
+                        jogadores[0].Cartas.Add(cartaJogador1);
+                    }
+                }
+                else if (TipoCartaJogador2 == 'T')
+                {
+                    if (TipoCartaJogador1 == 'A')
+                    {
+                        Console.WriteLine($"\nO Jogador 1 venceu! (Super Trunfo perde para carta A - {cartaJogador1.TipoCarta})");                    // Jogador 1 Recebe as cartas
+                        jogadores[0].Cartas.Add(cartaJogador2);
+                        jogadores[0].Cartas.Add(cartaJogador1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nO Jogador 2 venceu com o Super Trunfo!");
+                        // Jogador 2 Recebe as cartas
+                        jogadores[1].Cartas.Add(cartaJogador1);
+                        jogadores[1].Cartas.Add(cartaJogador2);
+
+                    }
+                }
+            }
+
+            else
+            {
+                Decimal AtributoJogador1;
+                Decimal AtributoJogador2;
+                AtributoJogador1 = cartaJogador1.ObterValorAtributo(opcao);
+                AtributoJogador2 = cartaJogador2.ObterValorAtributo(opcao);
+
+                Console.WriteLine($"\n{cartaJogador1.Nacao} {AtributoJogador1} X {cartaJogador2.Nacao} {AtributoJogador2}");
+
+                Console.WriteLine("");
+
+                if (AtributoJogador1 > AtributoJogador2)
+                {
+                    Console.WriteLine("O Jogador 1 venceu!");
+                    // Jogador 1 Recebe as cartas
+                    jogadores[0].Cartas.Add(cartaJogador2);
+                    jogadores[0].Cartas.Add(cartaJogador1);
+
+                }
+                else
+                {
+                    Console.WriteLine("O Jogador 2 venceu!");
+                    // Jogador 2 Recebe as cartas
+                    jogadores[1].Cartas.Add(cartaJogador2);
+                    jogadores[1].Cartas.Add(cartaJogador1);
+                }
+            }
+        }
+        public void Rodadas()
+        {
+            int rodada = 1;
+
+            // O jogo continua enquanto AMBOS tiverem cartas
+            while (jogadores[0].Cartas.Count > 0 && jogadores[1].Cartas.Count > 0)
+            {
+                Console.WriteLine($"\n================ RODADA {rodada} ================");
+                Console.WriteLine($"Placar de cartas -> J1: {jogadores[0].Cartas.Count} | J2: {jogadores[1].Cartas.Count}");
+                /*
+                foreach (var carta in jogadores[0].Cartas)
+                {
+                    Console.WriteLine($"{carta.TipoCarta} - {carta.Nacao} - U$ {carta.Pib} - {carta.Tamanho} - {carta.Populacao} - {carta.Idh}");
+                }
+                */
+                Console.WriteLine($"\nCarta do Jogador 1: {jogadores[0].Cartas[0].TipoCarta} - {jogadores[0].Cartas[0].Nacao} - PIB  U$ {jogadores[0].Cartas[0].Pib} - Extensão Territorial {jogadores[0].Cartas[0].Tamanho} - População {jogadores[0].Cartas[0].Populacao} - IDH {jogadores[0].Cartas[0].Idh}");
+
+                // Seleciona as cartas que vão pro final do baralho do vencedor
+                Carta cartaJogador1 = jogadores[0].Cartas[0];
+                Carta cartaJogador2 = jogadores[1].Cartas[0];
+
+                // Remove do topo da mão
+                jogadores[0].Cartas.RemoveAt(0);
+                jogadores[1].Cartas.RemoveAt(0);
+
+
+                 // Declarada fora para ser vista pelo restante do código
+
+                while (true)
+                {
+                    Console.WriteLine("\n---- Escolha um dos atributos ----");
+                    Console.WriteLine("""
+                    1 - PIB 
+                    2 - Tamanho do território
+                    3 - População
+                    4 - IDH
+                    """);
+
+                    try
+                    {
+                        opcao = int.Parse(Console.ReadLine()); // Aqui ela recebe o valor do usuário
+
+                        if (opcao >= 1 && opcao <= 4)
+                        {
+                            break; // Valor correto digitado, sai do loop e continua a rodada
+                        }
+
+                        Console.WriteLine("Opção inválida! Escolha apenas de 1 a 4.");
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Entrada inválida! Digite apenas o número da opção.");
+                    }
+                }
+                VerificarCartas(cartaJogador1,cartaJogador2,opcao);
+                
+             
+                rodada++;
+            }
+
+            // Quando o while termina, alguém ficou com 0 cartas:
+            Console.WriteLine("\n================ FIM DE JOGO ================");
+            if (jogadores[0].Cartas.Count > 0)
+            {
+                Console.WriteLine($"PARABÉNS! {jogadores[0].Nome} venceu a partida e conquistou todas as cartas!");
+            }
+            else
+            {
+                Console.WriteLine($"PARABÉNS! {jogadores[1].Nome} venceu a partida e conquistou todas as cartas!");
+            }
+
+            Console.WriteLine("\nDigite algo para sair ");
+            Console.ReadLine();
+        }
+        public void IniciarJogo(Baralho baralho)
+        {
+            MostrarBaralho(baralho);
+            baralho.DistribuirCartas(jogadores);
+            Rodadas();
+        }
+    }
+}
--- Paises_SuperTrunfo/Program.cs
+++ Paises_SuperTrunfo/Program.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 
 using Paises_SuperTrunfo;
 
-// 1. Instancia o baralho
+// 1. Instancia o baralho e a lógica do jogo
 Baralho baralho = new Baralho();
+LogicaDoJogo logicaDoJogo = new LogicaDoJogo();
 
 // 2. Popula com algumas cartas de teste
 baralho.AdicionarCarta(new Carta { Nacao = "Brasil", Pib = 1920000000000m, Tamanho = 8515767, Populacao = 214000000, Idh = 0.754f, IsSuperTrunfo = false, TipoCarta = "1A" });
@@ -21,165 +22,8 @@ baralho.AdicionarCarta(new Carta { Nacao = "Rússia", Pib = 2000000000000m, Tama
 baralho.AdicionarCarta(new Carta { Nacao = "Austrália", Pib = 1700000000000m, Tamanho = 7692024, Populacao = 27000000, Idh = 0.946f, IsSuperTrunfo = false, TipoCarta = "1D" });
 
 
-Console.WriteLine("--- Cartas antes de embaralhar ---");
-foreach (var carta in baralho.Cartas)
-{
-    Console.WriteLine($"{carta.TipoCarta} - {carta.Nacao}");
-}
-
-// 3. Embaralha as cartas
-baralho.Embaralhar();
-
-Console.WriteLine("\n--- Cartas depois de embaralhar ---");
-foreach (var carta in baralho.Cartas)
-{
-    Console.WriteLine($"{carta.TipoCarta} - {carta.Nacao}");
-}
-
-// 4. Cria jogadores e distribui as cartas
-List<Jogador> jogadores = new List<Jogador>
-            {
-                new Jogador { Nome = "Jogador 1" },
-                new Jogador { Nome = "Jogador 2" }
-            };
-
-baralho.DistribuirCartas(jogadores);
-
-int rodada = 1;
-
-// O jogo continua enquanto AMBOS tiverem cartas
-while (jogadores[0].Cartas.Count > 0 && jogadores[1].Cartas.Count > 0)
-{
-    Console.WriteLine($"\n================ RODADA {rodada} ================");
-    Console.WriteLine($"Placar de cartas -> J1: {jogadores[0].Cartas.Count} | J2: {jogadores[1].Cartas.Count}");
-    /*
-    foreach (var carta in jogadores[0].Cartas)
-    {
-        Console.WriteLine($"{carta.TipoCarta} - {carta.Nacao} - U$ {carta.Pib} - {carta.Tamanho} - {carta.Populacao} - {carta.Idh}");
-    }
-    */
-    Console.WriteLine($"\nCarta do Jogador 1: {jogadores[0].Cartas[0].TipoCarta} - {jogadores[0].Cartas[0].Nacao} - PIB  U$ {jogadores[0].Cartas[0].Pib} - Extensão Territorial {jogadores[0].Cartas[0].Tamanho} - População {jogadores[0].Cartas[0].Populacao} - IDH {jogadores[0].Cartas[0].Idh}");
-
-    // Seleciona as cartas que vão pro final do baralho do vencedor
-    Carta cartaJogador1 = jogadores[0].Cartas[0];
-    Carta cartaJogador2 = jogadores[1].Cartas[0];
-
-    // Remove do topo da mão
-    jogadores[0].Cartas.RemoveAt(0);
-    jogadores[1].Cartas.RemoveAt(0);
 
 
-    int opcao; // Declarada fora para ser vista pelo restante do código
 
-    while (true)
-    {
-        Console.WriteLine("\n---- Escolha um dos atributos ----");
-        Console.WriteLine("""
-        1 - PIB 
-        2 - Tamanho do território
-        3 - População
-        4 - IDH
-        """);
 
-        try
-        {
-            opcao = int.Parse(Console.ReadLine()); // Aqui ela recebe o valor do usuário
-
-            if (opcao >= 1 && opcao <= 4)
-            {
-                break; // Valor correto digitado, sai do loop e continua a rodada
-            }
-
-            Console.WriteLine("Opção inválida! Escolha apenas de 1 a 4.");
-        }
-        catch (FormatException)
-        {
-            Console.WriteLine("Entrada inválida! Digite apenas o número da opção.");
-        }
-    }
-
-        if (cartaJogador1.IsSuperTrunfo || cartaJogador2.IsSuperTrunfo)
-    {
-
-        char TipoCartaJogador1 = cartaJogador1.TipoCarta[^1];
-        char TipoCartaJogador2 = cartaJogador2.TipoCarta[^1];
-        if (TipoCartaJogador1 == 'T')
-        {
-            if (TipoCartaJogador2 == 'A')
-            {
-                Console.WriteLine($"\nO Jogador 2 venceu! (Super Trunfo perde para carta A - {cartaJogador2.TipoCarta})");
-
-                // Jogador 2 Recebe as cartas
-                jogadores[1].Cartas.Add(cartaJogador1);
-                jogadores[1].Cartas.Add(cartaJogador2);
-            }
-            else
-            {
-                Console.WriteLine("\nO Jogador 1 venceu com o Super Trunfo!");
-                // Jogador 1 Recebe as cartas
-                jogadores[0].Cartas.Add(cartaJogador2);
-                jogadores[0].Cartas.Add(cartaJogador1);
-            }
-        }
-        else if (TipoCartaJogador2 == 'T')
-        {
-            if (TipoCartaJogador1 == 'A')
-            {
-                Console.WriteLine($"\nO Jogador 1 venceu! (Super Trunfo perde para carta A - {cartaJogador1.TipoCarta})");                    // Jogador 1 Recebe as cartas
-                jogadores[0].Cartas.Add(cartaJogador2);
-                jogadores[0].Cartas.Add(cartaJogador1);
-            }
-            else
-            {
-                Console.WriteLine("\nO Jogador 2 venceu com o Super Trunfo!");
-                // Jogador 2 Recebe as cartas
-                jogadores[1].Cartas.Add(cartaJogador1);
-                jogadores[1].Cartas.Add(cartaJogador2);
-
-            }
-        }
-    }
-
-    else
-    {
-        Decimal AtributoJogador1;
-        Decimal AtributoJogador2;
-        AtributoJogador1 = cartaJogador1.ObterValorAtributo(opcao);
-        AtributoJogador2 = cartaJogador2.ObterValorAtributo(opcao);
-
-        Console.WriteLine($"\n{cartaJogador1.Nacao} {AtributoJogador1} X {cartaJogador2.Nacao} {AtributoJogador2}");
-
-        Console.WriteLine("");
-
-        if (AtributoJogador1 > AtributoJogador2)
-        {
-            Console.WriteLine("O Jogador 1 venceu!");
-            // Jogador 1 Recebe as cartas
-            jogadores[0].Cartas.Add(cartaJogador2);
-            jogadores[0].Cartas.Add(cartaJogador1);
-
-        }
-        else
-        {
-            Console.WriteLine("O Jogador 2 venceu!");
-            // Jogador 2 Recebe as cartas
-            jogadores[1].Cartas.Add(cartaJogador2);
-            jogadores[1].Cartas.Add(cartaJogador1);
-        }
-    }
-    rodada++;
-}
-
-// Quando o while termina, alguém ficou com 0 cartas:
-Console.WriteLine("\n================ FIM DE JOGO ================");
-if (jogadores[0].Cartas.Count > 0)
-{
-    Console.WriteLine($"PARABÉNS! {jogadores[0].Nome} venceu a partida e conquistou todas as cartas!");
-}
-else
-{
-    Console.WriteLine($"PARABÉNS! {jogadores[1].Nome} venceu a partida e conquistou todas as cartas!");
-}
-
-Console.WriteLine("\nDigite algo para sair ");
-Console.ReadLine();
+logicaDoJogo.IniciarJogo(baralho);
